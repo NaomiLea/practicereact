0a29028ba06d4dbc9f9446dc1059caf08f4f4f6b
NaomiLea <naomik@hometalk.com>
2018-11-22 15:27:12 +0200

    adding stuff

---
+++ src/header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Select from './selectgame.jsx'
+
+class Header extends React.Component {
+
+state = {name: null};
+
+  changeName(){
+
+    this.setState({name: prompt("What's your name?")});
+};
+
+  render() {
+
+    return <div>
+        <div className="App-header">
+            <h1>Welcome {this.state.name}</h1>
+            <button className="change" onClick={()=>this.changeName()} label="Change player">Change player</button>
+            <h2>Choose a game</h2>
+            <Select/>
+        </div>
+      </div>
+
+  }
+}
+
+export default Header;
---
+++ src/selectgame.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Select extends React.Component {
+  render() {
+    return (
+      <div>
+        <select>
+          <option>Game one</option>
+          <option>Game two</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default Select;
